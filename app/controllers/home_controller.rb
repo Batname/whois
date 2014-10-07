3cb33a17669e471bddf48fda98a5c717215a36ef
Batname <dadubinin@gmail.com>
2014-10-07 15:43:07 +0300

    home search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,5 +12,7 @@ class HomeController < ApplicationController
     @url = params[:q]
     @data_url = Whois.whois(@url)
     @reg_email = /\b([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+?)(\.[a-zA-Z.]*)\b/
+    @administrative = 'Administrative Contacts:'
+    @technical = 'Technical Contacts:'
   end
 end
